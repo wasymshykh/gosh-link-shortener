a1c4e728f1fcb6b14be2e81976e3bda8b16d18a8
Wasym <46717121+wasymshykh@users.noreply.github.com>
2020-08-11 20:51:12 +0500

    link input js and other fixes

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -27,7 +27,32 @@ $(document).ready(() => {
         })
     })
 
-    
+    /* link-input open link and copy button populator */
+
+    _l = $('.link-input');
+    _l.each((l) => {
+        el = _l[l]
+        lnk = $(el).find('input').val()
+        gt = $(el).find('.link-actions > .goto')
+        cp = $(el).find('.link-actions > .copy')
+        gt.attr('href', lnk)
+
+        /* copy click event */
+        $(cp).click((e)=>{
+            inp = $(e.currentTarget).parent().prev();
+            t = $("<input>");
+            $("body").append(t);
+            t.val(inp.val()).select();
+            document.execCommand("copy");
+            t.remove();
+
+            /* icon change to check then back to copy */
+            e.currentTarget.innerHTML = '<i class="fas fa-check"></i>';
+            setTimeout(() => {
+                e.currentTarget.innerHTML = '<i class="fas fa-copy"></i>';
+            }, 1000);
+        })
+    })
 
     
 })
